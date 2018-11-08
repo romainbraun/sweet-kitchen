6533364787a0a8e4734dc4bd5862355586f209e8
Romain Braun <romain@oldstlabs.com>
2018-11-08 00:31:58 +0000

    Working webpack configuration

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './components/App/App';
+
+declare let module:any;
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('mount'),
+  );
+});
+
+if (module.hot) {
+  module.hot.accept();
+}
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ["./src/index.tsx", "webpack-hot-middleware/client"]
+  },
+
+  output: {
+      filename: "[name].bundle.js",
+      path: __dirname + "/dist",
+      publicPath: '/'
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  mode: "development",
+
+  resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
+
+  module: {
+      rules: [
+          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+          { 
+            test: /\.tsx?$/, 
+            loader: "awesome-typescript-loader" 
+          },
+
+          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+          { 
+            enforce: "pre", 
+            test: /\.js$/, 
+            loader: "source-map-loader" 
+          }
+      ]
+  },
+
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: 'src/index.html'
+      }),
+      new webpack.HotModuleReplacementPlugin()
+  ],
+};
